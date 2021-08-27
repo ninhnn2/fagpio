1e043960924dbfa0305f3aabe20420df09a368af
ninhnn2 <fanningnguyen@gmail.com>
2021-08-27 22:13:55 +0700

    add control gpio input

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ OBJ_DIR = build_$(NAME_MODULE)
 CXX=/home/fanning/workspace/f1c100s/licheepi_nano_sdk/buildroot-2021.02.3/buildroot-2021.02.3/output/host/bin/arm-buildroot-linux-gnueabi-g++
 CC=/home/fanning/workspace/f1c100s/licheepi_nano_sdk/buildroot-2021.02.3/buildroot-2021.02.3/output/host/bin/arm-buildroot-linux-gnueabi-gcc
 
-#CXX=/home/fanning/workspace/f1c100s/licheepi_nano_sdk/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-g++
-#CC=/home/fanning/workspace/f1c100s/licheepi_nano_sdk/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc
-
 CFLAGS = -I.
 OBJ = $(OBJ_DIR)/fagpio.o
 IP_ADDR = 192.168.1.100
+
+#all: create $(OBJ_DIR)/$(NAME_MODULE)
 all: lib
+
 create:
 	@echo mkdir -p $(OBJ_DIR)
 	@mkdir -p $(OBJ_DIR)
@@ -30,11 +30,13 @@ lib:
 	$(CC) -c -Wall -Werror -fpic fagpio.c
 	$(CC) -shared -o libfagpio.so fagpio.o
 
-.PHONY: install
-install:
-	cp libfagpio.so /home/fanning/workspace/f1c100s/licheepi_nano_sdk/rootfs/lib/fagpio/
-	cp fagpio.h /home/fanning/workspace/f1c100s/licheepi_nano_sdk/rootfs/lib/fagpio/
+#.PHONY: install
+#install:
+#	cp libfagpio.so /home/fanning/workspace/f1c100s/licheepi_nano_sdk/rootfs/lib/fagpio/
+#	cp fagpio.h /home/fanning/workspace/f1c100s/licheepi_nano_sdk/rootfs/lib/fagpio/
 
 .PHONY: copy
 copy:
-	sshpass -p "000" scp -r /home/fanning/workspace/f1c100s/licheepi_nano_sdk/rootfs/lib/fagpio root@$(IP_ADDR):/rom/
+	sshpass -p "000" scp $(OBJ_DIR)/$(NAME_MODULE) root@$(IP_ADDR):/rom/work
+
+
--- fagpio.c
+++ fagpio.c
@@ -4,7 +4,6 @@ struct cpu_peripheral gpio = {GPIO_PAGE_OFFSET};
 
 // Exposes the physical address defined in the passed structure using mmap on /dev/mem
 int map_peripheral(struct cpu_peripheral *p) {
-	// Open /dev/mem
 	if ((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
 		printf("Failed to open /dev/mem, try checking permissions.\n");
 		return -1;
@@ -15,8 +14,8 @@ int map_peripheral(struct cpu_peripheral *p) {
 				BLOCK_SIZE,
 				PROT_READ|PROT_WRITE,
 				MAP_SHARED,
-				p->mem_fd,  // File descriptor to physical memory virtual file '/dev/mem'
-				p->addr_p      // Address in physical map that we want this memory block to expose
+				p->mem_fd,						// File descriptor to physical memory virtual file '/dev/mem'
+				p->addr_p						// Address in physical map that we want this memory block to expose
 				);
 
 	if (p->map == MAP_FAILED) {
@@ -33,36 +32,6 @@ void unmap_peripheral(struct cpu_peripheral *p) {
 	munmap(p->map, BLOCK_SIZE);
 	close(p->mem_fd);
 }
-
-//int main(void) {
-
-//	if(map_peripheral(&gpio) == -1) {
-//		printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-//		return -1;
-//	}
-
-//	pinMode(4, 1);
-//	pinMode(5, 1);
-//	pinMode(6, 1);
-//	pinMode(7, 1);
-
-//	while(1) {
-//		digitalWrite(4, 1);
-//		digitalWrite(5, 1);
-//		digitalWrite(6, 1);
-//		digitalWrite(7, 1);
-
-//		digitalWrite(4, 0);
-//		digitalWrite(5, 0);
-//		digitalWrite(6, 0);
-//		digitalWrite(7, 0);
-
-//	}
-
-//	unmap_peripheral(&gpio);
-
-//	return 0;
-//}
 
 /*
 PE_CFG0: PE Configure Register 0: Offset: 0x90
@@ -85,11 +54,11 @@ BIT
 */
 
 int fagpio_setup(void) {
-		if(map_peripheral(&gpio) == -1) {
-			printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-			return -1;
-		}
-		return 0;
+	if(map_peripheral(&gpio) == -1) {
+		printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
+		return -1;
+	}
+	return 0;
 }
 
 void fagpio_free(void) {
@@ -97,11 +66,53 @@ void fagpio_free(void) {
 }
 
 void pinMode(uint8_t Pin, uint8_t Mode) {
-	volatile unsigned int PE_CFG0;
-	PE_CFG0 = *(volatile unsigned int *)((unsigned char*)gpio.addr + PIO_ADDR_PORT + rPE_CFG0);
-	volatile unsigned int MASK_CONFIG = (~(15 << (4*Pin)));
-	volatile unsigned int MASK_X      = (1 << (4*Pin));
-	*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_CFG0)=((PE_CFG0 & MASK_CONFIG)|MASK_X);
+	if (0 == Mode) {
+		printf("Set output\n");
+		volatile unsigned int PE_CFG0;
+		PE_CFG0 = *(volatile unsigned int *)((unsigned char*)gpio.addr + PIO_ADDR_PORT + rPE_CFG0);
+		volatile unsigned int MASK_CONFIG = (~(15 << (4*Pin)));
+		volatile unsigned int MASK_X      = (1 << (4*Pin));
+
+		*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_CFG0)=((PE_CFG0 & MASK_CONFIG)|MASK_X);
+		printf("([OUTPUT] PE_CFG0 = %08X\n", *(volatile unsigned int *)((unsigned char*)gpio.addr + PIO_ADDR_PORT + rPE_CFG0));
+	} else if (1 == Mode) {
+		printf("Set input\n");
+		volatile unsigned int PE_CFG0, PE_PULL_REG;
+
+		PE_CFG0 = *(volatile unsigned int *)((unsigned char*)gpio.addr + PIO_ADDR_PORT + rPE_CFG0);
+
+		PE_PULL_REG = *(volatile unsigned int *)((unsigned char*)gpio.addr + PIO_ADDR_PORT + rPE_PULL0);
+
+		volatile unsigned int MASK_CONFIG = (~(15 << (4*Pin)));
+		volatile unsigned int MASK_X      = (0 << (4*Pin));
+
+		/*
+		clear 2bit to 0
+
+		Example:
+		00: Pull-up/down disable
+		01: Pull-up
+		10: Pull-down
+
+					   0xFFFFFFF = 0b 1111 1111 1111 1111 1111 1111 1111 1111 (32 Bit)
+					   F = 0b1111 (binary)
+					   0x1  1 | 1  1
+				  Bit    3  2 | 1  0
+							  | Bit 0 and 1, set pull up/down for PE0
+
+						 Bit 2 and 3, set pull up/down for PE1
+		*/
+
+		/* clear two bit to zero */
+		/* Example: PE2 => 3 << (2*2) => 3 << 4 => 0b00110000 => ~(0b00110000) => 0b11001111 */
+		/* volatile unsigned int MASK_PULL = 0b11001111 => 0b1111 1111 1111 1111 1111 1111 1100 1111 */
+
+		volatile unsigned int MASK_PULL    = (~(3 << (Pin*2)));
+		volatile unsigned int MASK_XPULL   = (1 << (Pin*2));
+
+		*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_CFG0) = ((PE_CFG0 & MASK_CONFIG)|MASK_X);
+		*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_PULL0) = ((PE_PULL_REG & MASK_PULL) | MASK_XPULL);
+	}
 }
 
 void digitalWrite(uint8_t pin, uint8_t value) {
@@ -109,16 +120,62 @@ void digitalWrite(uint8_t pin, uint8_t value) {
 		volatile unsigned int PE_DAT;
 		volatile unsigned int MASK_CONFIG = (~(1 << (pin)));
 		volatile unsigned int MASK_X      = (1 << (pin));
-		PE_DAT=*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_DAT);
+		PE_DAT = *(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_DAT);
 		*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET + rPE_DAT)=((PE_DAT & MASK_CONFIG)|MASK_X);
 	}
 	else if(value == 0) {
 		volatile unsigned int PE_DAT;
 		volatile unsigned int MASK_CONFIG = (~(1 << (pin)));
-		PE_DAT=*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_DAT);
+		PE_DAT = *(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET+rPE_DAT);
 		*(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET + rPE_DAT)=((PE_DAT & MASK_CONFIG));
 	}
 }
+
+uint8_t digitalRead(uint8_t pin) {
+	volatile unsigned int PE_DAT;
+
+	volatile uint8_t value;
+
+	PE_DAT = *(volatile unsigned int *)((unsigned char*)gpio.addr + GPIO_BASE_OFFSET + rPE_DAT);
+
+	value = (uint8_t)(((0x000000FF & PE_DAT) >> pin) & 0x1);
+
+	return value ;
+}
+
+//int main(void) {
+
+//	fagpio_setup();
+
+//	pinMode(2, 1);
+//	pinMode(3, 1);
+//	pinMode(4, 1);
+//	pinMode(5, 1);
+//	pinMode(6, 1);
+
+//	while(1) {
+//		sleep(1);
+//		printf("PE2 = %0d\n", digitalRead(2));
+//		printf("PE3 = %0d\n", digitalRead(3));
+//		printf("PE4 = %0d\n", digitalRead(4));
+//		printf("PE5 = %0d\n", digitalRead(5));
+//		printf("PE6 = %0d\n", digitalRead(6));
+//	}
+
+//	fagpio_free();
+
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
 
 
 
